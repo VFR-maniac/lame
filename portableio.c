1d296b104145b47b6185b6cb6c1524af6d2d6f7a
sbellon <sbellon>
2000-06-07 22:56:02 +0000

    added support for FPA10 hardware (RISC OS only)

--- portableio.c
+++ portableio.c
@@ -4,7 +4,7 @@
  * Warranty Information
  * Even though Apple has reviewed this software, Apple makes no warranty
  * or representation, either express or implied, with respect to this
- * software, its quality, accuracy, merchantability, or fitness for a 
+ * software, its quality, accuracy, merchantability, or fitness for a
  * particular purpose.  As a result, this software is provided "as is,"
  * and you, its user, are assuming the entire risk as to its quality
  * and accuracy.
@@ -43,17 +43,25 @@
  * conversions, and accommodated conversions involving +/- infinity,
  * NaN's, and denormalized numbers.
  *
- * $Id: portableio.c,v 1.1 1999/11/24 08:43:35 markt Exp $
+ * $Id: portableio.c,v 1.2 2000/06/07 22:56:02 sbellon Exp $
  *
  * $Log: portableio.c,v $
- * Revision 1.1  1999/11/24 08:43:35  markt
- * Initial revision
+ * Revision 1.2  2000/06/07 22:56:02  sbellon
+ * added support for FPA10 hardware (RISC OS only)
+ *
+ * Revision 1.1.1.1  1999/11/24 08:43:35  markt
+ * initial checkin of LAME
+ * Starting with LAME 3.57beta with some modifications
  *
  * Revision 2.6  91/04/30  17:06:02  malcolm
  */
 
 #include	<stdio.h>
+#if defined(__riscos__) && defined(FPA10)
+#include	"ymath.h"
+#else
 #include	<math.h>
+#endif
 #include	"portableio.h"
 
 /****************************************************************
