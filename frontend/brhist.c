95641404f52cceb0a8364b803436b43b931615d4
markt <markt>
2000-11-18 05:40:30 +0000

    added debian stuff.  Make one of the KLEMM's the default

--- frontend/brhist.c
+++ frontend/brhist.c
@@ -19,7 +19,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/* $Id: brhist.c,v 1.27 2000/11/07 01:59:05 pfk Exp $ */
+/* $Id: brhist.c,v 1.28 2000/11/18 05:40:31 markt Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -115,8 +115,8 @@ int  brhist_init ( const lame_global_flags* gf, const int bitrate_kbps_min, cons
     brhist.vbr_bitrate_min_index = calculate_index ( brhist.kbps, BRHIST_WIDTH, bitrate_kbps_min );
     brhist.vbr_bitrate_max_index = calculate_index ( brhist.kbps, BRHIST_WIDTH, bitrate_kbps_max );
 
-    if ( (unsigned)brhist.vbr_bitrate_min_index >= BRHIST_WIDTH  ||  
-         (unsigned)brhist.vbr_bitrate_max_index >= BRHIST_WIDTH ) {
+    if ( brhist.vbr_bitrate_min_index >= BRHIST_WIDTH  ||  
+         brhist.vbr_bitrate_max_index >= BRHIST_WIDTH ) {
 	fprintf ( Console_IO.Error_fp, "lame internal error: VBR min %d kbps or VBR max %d kbps not allowed.\n", bitrate_kbps_min, bitrate_kbps_max );
 	return -1;
     }
