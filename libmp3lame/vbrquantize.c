d0bbb5c674d7994f2a3645b475a6ad2ad8c1bb66
robert <robert>
2001-01-24 21:33:18 +0000

    quick-fix for code that was broken by Takehiro's last "bit optimization." in function calc_xmin(), quantize_pvt.c

--- libmp3lame/vbrquantize.c
+++ libmp3lame/vbrquantize.c
@@ -19,7 +19,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/* $Id: vbrquantize.c,v 1.29 2001/01/14 05:36:45 markt Exp $ */
+/* $Id: vbrquantize.c,v 1.30 2001/01/24 21:33:18 robert Exp $ */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
@@ -188,7 +188,7 @@ calc_sfb_noise(const FLOAT8 *xr, const FLOAT8 *xr34, const int bw, const int sf)
 #ifdef MAXQUANTERROR
   return xfsf;
 #else
-  return xfsf/bw;
+  return xfsf;//bw;
 #endif
 }
 
@@ -272,7 +272,7 @@ calc_sfb_noise_ave(const FLOAT8 *xr, const FLOAT8 *xr34, const int bw, const int
 #ifdef MAXQUANTERROR
     return xfsf_eq;
 #else
-    return xfsf_eq/bw;
+    return xfsf_eq;//bw;
 #endif
 }
 
