593ec58ad765d2e0019a1c72f08d32ba4d467c9d
markt <markt>
2001-02-25 22:47:38 +0000

    removed a klemm_42 from vorbis_interface.c

--- libmp3lame/vorbis_interface.c
+++ libmp3lame/vorbis_interface.c
@@ -1,4 +1,4 @@
-/* $Id: vorbis_interface.c,v 1.13 2001/02/25 22:47:10 markt Exp $ */
+/* $Id: vorbis_interface.c,v 1.14 2001/02/25 22:47:38 markt Exp $ */
 
 
 /* Compile lame with
@@ -12,7 +12,6 @@ export CONFIG_DEFS="-DUSE_FFT3DN"
 
 make clean
 ../configure
-#echo "#define KLEMM_42  1" >> config.h
 make
 
 You can also do this with the "--with-vorbis" options in configure.
