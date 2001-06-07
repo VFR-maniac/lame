3cf3eee6341de7770c7a34f0797e906a45d1eb66
robert <robert>
2001-06-07 22:06:48 +0000

    --ath-adjust 1 (Gaby's original code) was disabled, any reasons for that JD?

--- libmp3lame/encoder.c
+++ libmp3lame/encoder.c
@@ -19,7 +19,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/* $Id: encoder.c,v 1.53 2001/06/07 10:11:41 jd- Exp $ */
+/* $Id: encoder.c,v 1.54 2001/06/07 22:06:48 robert Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -106,9 +106,7 @@ adjust_ATH( lame_global_flags* const  gfp,
     /*  adjust ATH depending on range of maximum value
      */
     switch ( gfc->ATH->use_adjust ) {
-    default:
 
-#ifdef OLD_ATH_AUTO_ADJUST
     case  1:    /* by Gabriel Bouvigne */
         if      (0.5 < max_val / 32768) {       /* value above 50 % */
                 gfc->ATH->adjust = 1.0;         /* do not reduce ATH */
@@ -124,7 +122,6 @@ adjust_ATH( lame_global_flags* const  gfp,
                     gfc->ATH->adjust = 0.01;
         }
         break;
-#endif /* OLD_ATH_AUTO_ADJUST */
 
     case  2:    /* by Robert Hegemann */
       {
@@ -188,6 +185,10 @@ adjust_ATH( lame_global_flags* const  gfp,
             gfc->ATH->adjust_limit = adj_lim_new;
         }
       }
+        break;
+        
+    default:
+        gfc->ATH->adjust = 1.0;	/* no adjustment */
         break;
     }   /* switch */
 }
