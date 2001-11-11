757ca26c242ca5fa9f855d3d8f00df30c1879233
aleidinger <aleidinger>
2001-11-11 15:06:07 +0000

    add CVS Id

--- libmp3lame/bitstream.c
+++ libmp3lame/bitstream.c
@@ -17,6 +17,8 @@
  * License along with this library; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
+ *
+ * $Id: bitstream.c,v 1.46 2001/11/11 15:06:07 aleidinger Exp $
  */
 
 
