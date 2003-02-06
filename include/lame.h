64361a113e7e9364d9414e4e47390b6ea8253d0a
markt <markt>
2003-02-06 20:04:39 +0000

    updated comments about lame_init() from Kendrick Hamilton

--- include/lame.h
+++ include/lame.h
@@ -18,7 +18,7 @@
  * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-/* $Id: lame.h,v 1.127 2003/02/02 18:50:41 bouvigne Exp $ */
+/* $Id: lame.h,v 1.128 2003/02/06 20:04:39 markt Exp $ */
 
 #ifndef LAME_LAME_H
 #define LAME_LAME_H
@@ -119,9 +119,10 @@ typedef lame_global_flags *lame_t;
 
 /*
  * REQUIRED:
- * initialize the encoder.  sets default for all encoder paramters,
- * returns -1 if some malloc()'s failed
- * otherwise returns 0
+ * initialize the encoder.  sets default for all encoder parameters,
+ * returns NULL if some malloc()'s failed
+ * otherwise returns pointer to structure needed for all future
+ * API calls.
  */
 lame_global_flags * CDECL lame_init(void);
 /* obsolete version */
