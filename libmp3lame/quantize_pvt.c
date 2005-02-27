ca7600ec0d9ecb901e3134ee6eabe00c5465f6d4
bouvigne <bouvigne>
2005-02-27 22:03:03 +0000

    calc_noise: fixed an assertion failure in frames with no big_values

--- libmp3lame/quantize_pvt.c
+++ libmp3lame/quantize_pvt.c
@@ -19,7 +19,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/* $Id: quantize_pvt.c,v 1.126 2005/02/27 20:43:10 bouvigne Exp $ */
+/* $Id: quantize_pvt.c,v 1.127 2005/02/27 22:03:03 bouvigne Exp $ */
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
@@ -651,7 +651,6 @@ FLOAT calc_noise_core_c( const gr_info * const cod_info,
     const int *ix = cod_info->l3_enc;
 
     assert(cod_info->count1 != 0);
-    assert(cod_info->big_values != 0);
 
     if (j> cod_info->count1) {
 	    while (l--) {
@@ -712,7 +711,6 @@ FLOAT calc_noise_core_sse( const gr_info * const cod_info,
     _noise._m128 = _mm_set_ps1(0);
 
     assert(cod_info->count1 != 0);
-    assert(cod_info->big_values != 0);
 
     _mm_prefetch((char*)cod_info->xr, _MM_HINT_T0);
     _mm_prefetch((char*)ix, _MM_HINT_T0);
