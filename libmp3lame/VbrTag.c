af683581cde830ba29772a709d12b4372865881d
bouvigne <bouvigne>
2005-09-01 18:12:17 +0000

    320kbps cbr was flagged as "non optimal" in the info header

--- libmp3lame/VbrTag.c
+++ libmp3lame/VbrTag.c
@@ -19,7 +19,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/* $Id: VbrTag.c,v 1.79 2005/03/08 23:07:45 robert Exp $ */
+/* $Id: VbrTag.c,v 1.80 2005/09/01 18:12:17 bouvigne Exp $ */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
@@ -682,7 +682,7 @@ int PutLameVBR(lame_global_flags *gfp, FILE *fpStream, uint8_t *pbtStreamBuffer,
 		gfp->short_blocks == short_block_dispensed 		||
 		((gfp->lowpassfreq == -1) && (gfp->highpassfreq == -1))	|| /* "-k" */
 		(gfp->scale_left != gfp->scale_right)			||
-		gfp->disable_reservoir		||
+		(gfp->disable_reservoir	 && gfp->brate < 320)	||
 		gfp->noATH			||
 		gfp->ATHonly			||
 		(nAthType == 0)    ||
