a10432a464be1f70e8dea6c8dbba7eeb71524a80
bouvigne <bouvigne>
2005-10-31 13:01:57 +0000

    fixed bug 1337556

--- libmp3lame/lame.c
+++ libmp3lame/lame.c
@@ -20,7 +20,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/* $Id: lame.c,v 1.280 2005/09/20 09:19:04 robert Exp $ */
+/* $Id: lame.c,v 1.281 2005/10/31 13:01:57 bouvigne Exp $ */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
@@ -240,7 +240,26 @@ optimum_samplefreq(int lowpassfreq, int input_samplefreq)
  *  - if possible, sfb21 should NOT be used
  *
  */
-    int suggested_samplefreq = input_samplefreq;
+    int suggested_samplefreq;
+    
+    if (input_samplefreq >= 48000)
+        suggested_samplefreq = 48000;
+    else if (input_samplefreq >= 44100)
+        suggested_samplefreq = 44100;
+    else if (input_samplefreq >= 32000)
+        suggested_samplefreq = 32000;
+    else if (input_samplefreq >= 24000)
+        suggested_samplefreq = 24000;
+    else if (input_samplefreq >= 22050)
+        suggested_samplefreq = 22050;
+    else if (input_samplefreq >= 16000)
+        suggested_samplefreq = 16000;
+    else if (input_samplefreq >= 12000)
+        suggested_samplefreq = 12000;
+    else if (input_samplefreq >= 11025)
+        suggested_samplefreq = 11025;
+    else if (input_samplefreq >= 8000)
+        suggested_samplefreq = 8000;
 
     if (lowpassfreq == -1)
         return suggested_samplefreq;
@@ -661,7 +680,7 @@ lame_init_params(lame_global_flags * const gfp)
        if we can't store them */
     if (!gfp->bWriteVbrTag){
         gfp->findReplayGain = 0;
-	gfp->decode_on_the_fly = 0;
+        gfp->decode_on_the_fly = 0;
         gfc->findPeakSample = 0;
     }
     gfc->findReplayGain = gfp->findReplayGain;
