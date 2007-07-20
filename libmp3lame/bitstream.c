12421a651fff8afee6669d5e89ec8bd68ad44fb7
robert <robert>
2007-07-20 23:23:34 +0000

    fixing task tracker item: [ 1755981 ] Percent signs in print format not escaped

--- libmp3lame/bitstream.c
+++ libmp3lame/bitstream.c
@@ -19,7 +19,7 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- * $Id: bitstream.c,v 1.77 2007/05/13 18:31:56 robert Exp $
+ * $Id: bitstream.c,v 1.78 2007/07/20 23:23:34 robert Exp $
  */
 
 
@@ -931,9 +931,9 @@ format_bitstream(lame_global_flags const *gfp)
                bits, bits % 8, bitsPerFrame);
 
         ERRORF(gfc, "This is a fatal error.  It has several possible causes:");
-        ERRORF(gfc, "90%  LAME compiled with buggy version of gcc using advanced optimizations");
-        ERRORF(gfc, " 9%  Your system is overclocked");
-        ERRORF(gfc, " 1%  bug in LAME encoding library");
+        ERRORF(gfc, "90%%  LAME compiled with buggy version of gcc using advanced optimizations");
+        ERRORF(gfc, " 9%%  Your system is overclocked");
+        ERRORF(gfc, " 1%%  bug in LAME encoding library");
 
         gfc->ResvSize = l3_side->main_data_begin * 8;
     };
