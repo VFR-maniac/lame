c2f0e445b14178d988dfe76ab4b2fd7696a5ff86
robert <robert>
2008-10-21 22:35:23 +0000

    replacing mpglib by libhip

--- mpglib/dct64_i386.c
+++ mpglib/dct64_i386.c
@@ -1,4 +1,21 @@
 /*
+ * Copyright (C) 2000 Albert L. Faber
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
  * Discrete Cosine Tansform (DCT) for subband synthesis
  * optimized for machines with no auto-increment. 
  * The performance is highly compiler dependend. Maybe
@@ -6,7 +23,7 @@
  * even for Intel processors.
  */
 
-/* $Id: dct64_i386.c,v 1.10 2004/04/14 22:15:44 robert Exp $ */
+/* $Id: dct64_i386.c,v 1.11 2008/10/21 22:35:23 robert Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
--- mpglib/tabinit.c
+++ mpglib/tabinit.c
@@ -1,4 +1,22 @@
-/* $Id: tabinit.c,v 1.12 2004/04/14 22:15:44 robert Exp $ */
+/*
+ * Copyright (C) 2000 Albert L. Faber
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+/* $Id: tabinit.c,v 1.13 2008/10/21 22:35:23 robert Exp $ */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
