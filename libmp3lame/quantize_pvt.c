9ec5db068598faa2991d9babdb0112dc308420ad
robert <robert>
2012-02-07 13:36:35 +0000

    - workaround for cygwin build problem - fix "fast floating point math" problem (possible bitrate bloat on quiet pieces)

--- libmp3lame/quantize_pvt.c
+++ libmp3lame/quantize_pvt.c
@@ -22,7 +22,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/* $Id: quantize_pvt.c,v 1.172 2012/01/08 23:50:21 robert Exp $ */
+/* $Id: quantize_pvt.c,v 1.173 2012/02/07 13:36:35 robert Exp $ */
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
@@ -645,7 +645,9 @@ calc_xmin(lame_internal_flags const *gfc,
                     xmin = x;
             }
         }
-        *pxmin++ = Max(xmin, 1e-20f);
+        xmin = Max(xmin, DBL_EPSILON);
+        cod_info->energy_above_cutoff[gsfb] = (en0 > xmin+1e-14f) ? 1 : 0;
+        *pxmin++ = xmin;
     }                   /* end of long block loop */
 
 
@@ -733,7 +735,9 @@ calc_xmin(lame_internal_flags const *gfc,
                         xmin = x;
                 }
             }
-            *pxmin++ = Max(xmin, 1e-20f);
+            xmin = Max(xmin, DBL_EPSILON);
+            cod_info->energy_above_cutoff[gsfb+b] = (en0 > xmin+1e-14f) ? 1 : 0;
+            *pxmin++ = xmin;
         }               /* b */
         if (cfg->use_temporal_masking_effect) {
             if (pxmin[-3] > pxmin[-3 + 1])
